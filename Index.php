36e15ed2435f2f747cf769b751f5d619e2c97eef
Noah <6035964@mborijnland.nl>
2024-04-22 16:40:20 +0200

    home pagina aangemaakt niet af

--- Index.php
+++ Index.php
@@ -1,18 +1,29 @@
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="keywords" content="">
-    <meta name="author" content="Luke, Noah">
-    <link rel="icon" type="image/x-icon" href="./assetds/images/OIG4.jpeg">
-    <link rel="stylesheet" href="./css/index.css">
-    <title>Mijn Buuf - Progressie vragen lijst</title>
+    <meta name="author" content="Luke, Noah, Malik, Walid, ">
+    <meta name="description" content="Een formulier/vragenlijst">
+    <meta name="keywords"
+        content="Gezondheid, Wonen, Sociale contacten, Werken, Taal">
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="icon" href="images/icon.png" type="image/x-icon">
+    <title>Mijn Buuf Formulier</title>
 </head>
-<body> 
 
-<script type='module' src='./js/index.js'></script>
+<body>
+    <header>
+        <?php include 'header.php'; ?>
+    </header>
+    <main>
+        <!-- de main zal hier plaats vinden -->
+    </main>
+
+    <footer>
+        <?php include 'footer.php'; ?>
+    </footer>
 </body>
+
 </html>
