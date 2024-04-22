4e92b452931f4a1ab903cdca1defe7fb8a37dc16
= <lukehoogenboom@gmail.com>
2024-04-22 14:45:29 +0200

    added pichai-UX

--- Index.php
+++ Index.php
@@ -1,1 +1,18 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Luke, Noah">
+    <link rel="icon" type="image/x-icon" href="./assets/54947833 (1).png">
+    <link rel="stylesheet" href="./css/index.css">
+    <title>Mijn Buuf - Progressie vragen lijst</title>
+</head>
+<body> 
+
+<script src='./js/index.js'></script>
+</body>
+</html>
