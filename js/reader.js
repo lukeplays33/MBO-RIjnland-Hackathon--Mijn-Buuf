0ff7514568cd28ba15b2a83279b4541a8dcffeab
= <lukehoogenboom@gmail.com>
2024-04-25 16:53:31 +0200

    last push

--- js/reader.js
+++ js/reader.js
@@ -1,10 +1,13 @@
-function opslaan() {const form = document.getElementById("vragenlijst");
-const submitter = document.querySelector("button[value=save]");
-const formData = new FormData(vragenlijst, submitter);
+function opslaan(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  const form = document.getElementById("vragenlijst");
+  const submitter = document.querySelector(".verder");
+  const formData = new FormData(vragenlijst, submitter);
 
-const output = document.getElementById("output");
+  const output = document.getElementById("output");
 
-for (const [key, value] of formData) {
-  output.textContent += `${key}: ${value}\n`;
-} 
+  for (const [key, value] of formData) {
+    output.textContent += `${key}: ${value}\n`;
+  }
 }
